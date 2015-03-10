d204bdb0726a8b45319c4d982a6a195a6cd2097c
Matt <MattBDev@users.noreply.github.com>
2015-03-10 15:40:03 -0400

    Update PlotMe
    Not building upon the latest version of PlotMe or even updating your plugin can cause major compatibility issues.

--- src/com/instancedev/skins/utils/PlotMeSupport.java
+++ src/com/instancedev/skins/utils/PlotMeSupport.java
@@ -1,30 +1,32 @@
 package com.instancedev.skins.utils;
 
+import com.worldcretornica.plotme_core.Plot;
+import com.worldcretornica.plotme_core.PlotMeCoreManager;
+import com.worldcretornica.plotme_core.api.IPlayer;
+import com.worldcretornica.plotme_core.bukkit.PlotMe_CorePlugin;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
-import com.worldcretornica.plotme.Plot;
-import com.worldcretornica.plotme.PlotManager;
-import com.worldcretornica.plotme.PlotMe;
-
 public class PlotMeSupport {
 
-	public static PlotMe getPlotMe() {
+	public static PlotMe_CorePlugin getPlotMe() {
 		Plugin plugin = Bukkit.getServer().getPluginManager().getPlugin("PlotMe");
 
-		if (plugin == null || !(plugin instanceof PlotMe)) {
+		if (plugin == null || !(plugin instanceof PlotMe_CorePlugin)) {
 			return null;
 		}
 
-		return (PlotMe) plugin;
+		return (PlotMe_CorePlugin) plugin;
 	}
 	
 	public static boolean canBuildPlotMe(Player p, Location l) {
-		String id = PlotManager.getPlotId(l);
-		if (!id.equalsIgnoreCase("")) {
-			Plot plot = PlotManager.getMap(p).plots.get(id);
+		final PlotMeCoreManager manager = PlotMeCoreManager.getInstance();
+		IPlayer player = getPlotMe().wrapPlayer(p);
+		String id = manager.getPlotId(player);
+		if (!id.isEmpty()) {
+			Plot plot = manager.getMap(player).getPlot(id);
 			if (plot == null) {
 				if (!p.isOp()) {
 					return false;
